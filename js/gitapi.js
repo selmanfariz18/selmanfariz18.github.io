e4d7f6a1056522f1cfc2f22ae6b12dba258382cd
selmanfariz18 <salmanulfarizyka@gmail.com>
2023-10-23 19:56:47 +0530

    updated third year

--- js/gitapi.js
+++ js/gitapi.js
@@ -2,27 +2,27 @@
 async function getData(){
     const res=await fetch('https://api.github.com/users/selmanfariz18')
     const data=await res.json()
-    
+
     const card5 =document.getElementById('stat1')
     const name =document.createElement('h3')
     name.textContent=data.name
 
     card5.appendChild(name)
 
-    
+
     const card =document.getElementById('stat2')
     const repo_name =document.createElement('p')
     repo_name.textContent= data.public_repos
     card.appendChild(repo_name)
 
-    
+
     const card2 =document.getElementById('stat3')
     const folo_name =document.createElement('p')
     folo_name.textContent= data.followers
 
     card2.appendChild(folo_name)
 
-    
+
     const card3 =document.getElementById('stat4')
     const folow_name =document.createElement('p')
     folow_name.textContent= data.following
@@ -39,4 +39,4 @@ async function getData(){
     card4.appendChild(org_name)
 
 }
-getData();    
+getData();
